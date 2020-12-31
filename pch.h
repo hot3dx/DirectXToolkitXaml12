cad1b2421b6f3deb9ee050dea24289826eb03665
hot3dx <hot3dx@hotmail.com>
2020-12-31 11:29:27 -0700

    Add files via upload
    12-31-2020 changes to VS 2019 C++ Compiler and Linker setting to include, changes to the pch.h includes, this means the *.vcxproj and ^,vcxproj.filter files changed as well

--- pch.h
+++ pch.h
@@ -9,17 +9,7 @@
 
 #pragma once
 
-#pragma warning(disable : 4006 4264 4619 4061 4265 4355 4365 4571 4623 4625 4626 4628 4668 4710 4711 4746 4774 4820 4987 5026 5027 5031 5032 5039 5045)
-
-#include "targetver.h"
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-
-#include <windows.h>
-
-
+#include <ppltasks.h>
 
 #include <wrl.h>
 
@@ -66,11 +56,6 @@
 #include <utility>
 #include <vector>
 
-#pragma warning(push)
-#pragma warning(disable : 4702)
-#include <functional>
-#pragma warning(pop)
-
 #include <malloc.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -81,7 +66,9 @@
 #include <xaudio2.h>
 #include <xaudio2fx.h>
 #include <x3daudio.h>
+#include <xapo.h>
 #include <xapofx.h>
+#include <hrtfapoapi.h>
 
 #include <mmreg.h>
 #include <mfidl.h>
